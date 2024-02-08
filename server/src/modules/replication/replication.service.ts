53f6cdd4b422ce8f2f3cb94fb9a2265fa8291deb
Harshith Mullapudi <harshithmullapudi@gmail.com>
2024-02-08 22:02:25 +0530

    Fix: create replication slot in the replication service

--- server/src/modules/replication/replication.service.ts
+++ server/src/modules/replication/replication.service.ts
@@ -1,7 +1,7 @@
 /* eslint-disable dot-location */
 /** Copyright (c) 2024, Tegon, all rights reserved. **/
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Client } from 'pg';
 import {
@@ -11,44 +11,77 @@ import {
 
 import { SyncGateway } from 'modules/sync/sync.gateway';
 
-const REPLICATION_SLOT_NAME = 'repl';
-// const REPLICATION_SLOT_PLUGIN = 'wal2json';
+const REPLICATION_SLOT_NAME = 'tegon_replication_slot';
+const REPLICATION_SLOT_PLUGIN = 'wal2json';
 
 @Injectable()
 export default class ReplicationService {
   client: Client;
+  private readonly logger: Logger = new Logger('ReplicationService');
 
   constructor(
     private configService: ConfigService,
     private syncGateway: SyncGateway,
   ) {
     this.client = new Client({
-      connectionString: this.configService.get('REPLICATION_DATABASE_URL'),
+      user: configService.get('POSTGRES_USER'),
+      host: configService.get('DB_HOST'),
+      database: configService.get('POSTGRES_DB'),
+      password: configService.get('POSTGRES_PASSWORD'),
+      port: configService.get('DB_PORT'),
     });
   }
 
   async init() {
-    // await this.createReplicationSlot();
-    await this.setupReplication();
+    await this.createReplicationSlot();
+    // await this.setupReplication();
   }
 
-  // async createReplicationSlot() {
-  //   try {
-  //     await this.client.connect();
+  async deleteSlot() {
+    try {
+      const deleteReplicationSlotQuery = `SELECT pg_drop_replication_slot('${REPLICATION_SLOT_NAME}')`;
 
-  //     // Create replication slot
-  //     const result = await this.client.query(
-  //       'SELECT * FROM pg_create_logical_replication_slot($1, $2)',
-  //       [REPLICATION_SLOT_NAME, { plugin: REPLICATION_SLOT_PLUGIN }],
-  //     );
+      await this.client.query(deleteReplicationSlotQuery);
+    } catch (err) {
+      this.logger.error(err);
+    }
+  }
 
-  //     console.log('Replication slot created successfully:', result.rows[0]);
-  //   } catch (error) {
-  //     console.error('Error creating replication slot:', error);
-  //   } finally {
-  //     await this.client.end();
-  //   }
-  // }
+  async checkForSlot() {
+    const checkReplicationSlotQuery = `
+    SELECT * FROM pg_replication_slots WHERE slot_name = '${REPLICATION_SLOT_NAME}'
+  `;
+
+    const checkSlotResult = await this.client.query(checkReplicationSlotQuery);
+
+    if (checkSlotResult.rows.length > 0) {
+      await this.deleteSlot();
+    }
+  }
+
+  async createReplicationSlot() {
+    try {
+      await this.client.connect();
+
+      await this.checkForSlot();
+
+      const createReplicationSlotQuery = `
+        SELECT * FROM pg_create_logical_replication_slot(
+          '${REPLICATION_SLOT_NAME}',
+          '${REPLICATION_SLOT_PLUGIN}'
+        )
+      `;
+
+      // Create replication slot
+      const result = await this.client.query(createReplicationSlotQuery);
+
+      this.logger.log('Replication slot created successfully:', result.rows[0]);
+    } catch (error) {
+      this.logger.error('Error creating replication slot:', error);
+    } finally {
+      await this.client.end();
+    }
+  }
 
   async setupReplication() {
     const clientConfig = {
@@ -61,17 +94,13 @@ export default class ReplicationService {
     service
       .subscribe(plugin, REPLICATION_SLOT_NAME)
       .catch((e) => {
-        console.error(e);
+        this.logger.error(e);
       })
       .then(() => {
-        console.log('connected');
+        this.logger.log('Replication server connected');
       });
 
     service.on('data', (_lsn, log) => {
-      console.log(_lsn);
-      // log contains change data in JSON format
-      console.log(JSON.stringify(log));
-
       this.syncGateway.wss
         .to('d81f6173-60cf-46bc-bcca-4ab2921a52c3')
         .emit('message', JSON.stringify(log.change));
