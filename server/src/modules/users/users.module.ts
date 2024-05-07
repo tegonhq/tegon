231ebf8e4e79ed3eaf1949bfc5f1ceb9d7ecee27
Manoj K <saimanoj58@gmail.com>
2024-05-07 17:12:35 +0530

    Add: change password functionality

--- server/src/modules/users/users.module.ts
+++ server/src/modules/users/users.module.ts
@@ -1,14 +1,17 @@
 /** Copyright (c) 2024, Tegon, all rights reserved. **/
 
 import { Module } from '@nestjs/common';
+import { PrismaModule, PrismaService } from 'nestjs-prisma';
+
+import { SupertokensService } from 'modules/auth/supertokens/supertokens.service';
 
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, PrismaService, SupertokensService],
   exports: [UsersService],
 })
 export class UsersModule {}
--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -1,10 +1,15 @@
 /** Copyright (c) 2024, Tegon, all rights reserved. **/
 
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'nestjs-prisma';
+import supertokens from 'supertokens-node';
+import { SessionContainer } from 'supertokens-node/recipe/session';
+
+import { SupertokensService } from 'modules/auth/supertokens/supertokens.service';
 
 import { PublicUser, UpdateUserBody, userSerializer } from './user.interface';
+
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
@@ -71,5 +76,62 @@ export class UsersService {
       },
     });
     return userSerializer(user);
+  }
+
+  async changePassword(
+    supertokensService: SupertokensService,
+    userId: string,
+    session: SessionContainer,
+    passwordBody: Record<string, string>,
+  ) {
+    const oldPassword = passwordBody.oldPassword;
+    const newPassword = passwordBody.newPassword;
+
+    const userInfo = await supertokens.getUser(userId);
+
+    if (userInfo === undefined) {
+      throw new BadRequestException('User not found');
+    }
+
+    const loginMethod = userInfo.loginMethods.find(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (lM: any) =>
+        lM.recipeUserId.getAsString() ===
+          session.getRecipeUserId().getAsString() &&
+        lM.recipeId === 'emailpassword',
+    );
+
+    if (loginMethod === undefined) {
+      throw new BadRequestException(`You've signed up with different method`);
+    }
+    const email = loginMethod.email;
+
+    const EmailPassword = supertokensService.getEmailPasswordRecipe();
+
+    const isPasswordValid = await EmailPassword.signIn(
+      session!.getTenantId(),
+      email,
+      oldPassword,
+    );
+
+    if (isPasswordValid.status !== 'OK') {
+      throw new BadRequestException(`Your current password didn't match`);
+    }
+
+    // update the user's password using updateEmailOrPassword
+    const response = await EmailPassword.updateEmailOrPassword({
+      recipeUserId: session.getRecipeUserId(),
+      password: newPassword,
+      tenantIdForPasswordPolicy: session.getTenantId(),
+    });
+
+    if (response.status === 'PASSWORD_POLICY_VIOLATED_ERROR') {
+      // TODO: handle incorrect password error
+      throw new BadRequestException(
+        `Your new password didn't match with the policy`,
+      );
+    }
+
+    return { message: 'Successful' };
   }
 }
