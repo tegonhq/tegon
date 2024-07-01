7afbac4170eeb484f9d5dfc374f6440ada304c7c
Harshith Mullapudi <harshithmullapudi@gmail.com>
2024-07-01 15:41:38 +0530

    Feat: add website deployment

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
   async redirects() {
     return [
       {
