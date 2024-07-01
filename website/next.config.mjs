5e26f74c4d514364b35207dabc5a2e4285c01729
Harshith Mullapudi <harshithmullapudi@gmail.com>
2024-07-01 15:34:09 +0530

    Feat: added website

---
+++ website/next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
+};
+export default nextConfig;
