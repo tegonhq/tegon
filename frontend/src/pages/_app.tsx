6302d35b66b6fad7bca4a719c7f5d97ce9b898aa
Harshith Mullapudi <harshithmullapudi@gmail.com>
2024-07-03 14:35:08 +0530

    Fix: default to light

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -43,7 +43,7 @@ export const MyApp: NextComponentType<
     <SuperTokensWrapper>
       <ThemeProvider
         attribute="class"
-        defaultTheme="dark"
+        defaultTheme="light"
         enableSystem
         disableTransitionOnChange
       >
