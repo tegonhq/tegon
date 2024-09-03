02a61f0b6bf129ca4247d5aeb2dc39836b56b6b2
Harshith Mullapudi <harshithmullapudi@gmail.com>
2024-09-03 10:12:33 +0530

    Fix: update version trigger

--- apps/webapp/src/common/icon-utils.ts
+++ apps/webapp/src/common/icon-utils.ts
@@ -1,7 +1,9 @@
+import { RiMailFill } from '@remixicon/react';
 import { SlackIcon } from '@tegonhq/ui/icons';
 
 export const ICON_MAPPING = {
   slack: SlackIcon,
+  email: RiMailFill,
 };
 
 export type IconType = keyof typeof ICON_MAPPING;
