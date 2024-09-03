3d2ed72646693499551228674741a8a74d658d3c
Harshith Mullapudi <harshithmullapudi@gmail.com>
2024-09-03 09:52:12 +0530

    Fix: website layout

--- apps/website/tailwind.config.ts
+++ apps/website/tailwind.config.ts
@@ -1,1 +1,17 @@
-export * from '@tegonhq/ui/tailwind.config';
+/* eslint-disable import/no-anonymous-default-export */
+import config from '@tegonhq/ui/tailwind.config';
+
+export default {
+  ...config,
+  theme: {
+    ...config.theme,
+    fontSize: {
+      xs: '13px',
+      sm: '14px',
+      base: '15px',
+      md: '17px',
+      lg: '19px',
+      xl: '26px',
+    },
+  },
+};
