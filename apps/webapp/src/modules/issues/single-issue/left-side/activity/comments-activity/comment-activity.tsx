7b705c164558d374756a5de6b0c44e4dc237fd1b
dhruv Aggarwal <dhruvaggarwal@dhruvs-Laptop.local>
2024-12-14 22:31:30 +0530

    improved code quality by running pnpm lint command

--- apps/webapp/src/modules/issues/single-issue/left-side/activity/comments-activity/comment-activity.tsx
+++ apps/webapp/src/modules/issues/single-issue/left-side/activity/comments-activity/comment-activity.tsx
@@ -1,18 +1,17 @@
 import { TimelineItem } from '@tegonhq/ui/components/timeline';
+import { observer } from 'mobx-react-lite';
 import * as React from 'react';
 
-import {
-  GenericCommentActivity,
-  type GenericCommentActivityProps,
-} from './generic-comment-activity';
-import { SyncCommentActivity } from './sync-comment-activity';
 import type { IssueCommentType, User } from 'common/types';
-import { observer } from 'mobx-react-lite';
+
 import { useContextStore } from 'store/global-context-provider';
 
+import { GenericCommentActivity } from './generic-comment-activity';
+import { SyncCommentActivity } from './sync-comment-activity';
+
 interface GenericCommentActivityPropsv2 {
-  issueId: String;
-  commentId: String;
+  issueId: string;
+  commentId: string;
   user: User;
   childComments: IssueCommentType[];
   allowReply?: boolean;
@@ -21,32 +20,35 @@ interface GenericCommentActivityPropsv2 {
   hasMore?: boolean;
 }
 
-export const CommentActivity= observer((props: GenericCommentActivityPropsv2) => {
-  const { issueId, commentId, hasMore } = props;
-  const { commentsStore } = useContextStore();
+export const CommentActivity = observer(
+  (props: GenericCommentActivityPropsv2) => {
+    const { issueId, commentId, hasMore } = props;
+    const { commentsStore } = useContextStore();
 
-  const comment = commentsStore
-  .getComments(issueId)
-  .find((comment: { id: string }) => comment.id === commentId);
-  
-  const sourceMetadata =comment && comment.sourceMetadata
-    ? JSON.parse(comment.sourceMetadata)
-    : undefined;
-  if(comment===undefined){
-    return null
-  }
+    const comment = commentsStore
+      .getComments(issueId)
+      .find((comment: { id: string }) => comment.id === commentId);
 
-  if (sourceMetadata && sourceMetadata.type) {
-    return <SyncCommentActivity comment={comment} {...props} />;
-  }
+    const sourceMetadata =
+      comment && comment.sourceMetadata
+        ? JSON.parse(comment.sourceMetadata)
+        : undefined;
+    if (comment === undefined) {
+      return null;
+    }
 
-  return (
-    <TimelineItem
-      className="w-full"
-      key={`${comment.id}-comment`}
-      hasMore={hasMore}
-    >
-      <GenericCommentActivity comment={comment}  {...props} />
-    </TimelineItem>
-  );
-})
+    if (sourceMetadata && sourceMetadata.type) {
+      return <SyncCommentActivity comment={comment} {...props} />;
+    }
+
+    return (
+      <TimelineItem
+        className="w-full"
+        key={`${comment.id}-comment`}
+        hasMore={hasMore}
+      >
+        <GenericCommentActivity comment={comment} {...props} />
+      </TimelineItem>
+    );
+  },
+);
--- apps/webapp/src/modules/issues/single-issue/left-side/activity/comments-activity/comments-activity.tsx
+++ apps/webapp/src/modules/issues/single-issue/left-side/activity/comments-activity/comments-activity.tsx
@@ -1,6 +1,8 @@
 import { Timeline } from '@tegonhq/ui/components/timeline';
 // import { sort } from 'fast-sort';
+import { sort } from 'fast-sort';
 import { observer } from 'mobx-react-lite';
+import { useEffect, useState } from 'react';
 
 import type { User } from 'common/types';
 import type { IssueCommentType } from 'common/types';
@@ -12,78 +14,74 @@ import { useContextStore } from 'store/global-context-provider';
 
 import { CommentActivity } from './comment-activity';
 import { IssueComment } from './issue-comment';
-import { useEffect, useState } from 'react';
-import { sort } from 'fast-sort';
 
 interface CommentsActivityProps {
   commentOrder?: number;
 }
 
-export const CommentsActivity = observer(({ commentOrder = -1 }: CommentsActivityProps) => {
-  const issue = useIssueData();
-  const { commentsStore } = useContextStore();
+export const CommentsActivity = observer(
+  ({ commentOrder = -1 }: CommentsActivityProps) => {
+    const issue = useIssueData();
+    const { commentsStore } = useContextStore();
 
-  // State to manage sorted comments
-  const [sortedComments, setSortedComments] = useState<IssueCommentType[]>([]);
-
-  const { users, isLoading } = useUsersData(true);
-
-  useEffect(() => {
-    // Fetch and sort comments whenever the order or comments change
-    const comments = commentsStore.getComments(issue.id);
-    let sorted = [...comments]
-    sorted = sort(sorted)[
-      commentOrder > 0
-        ? 'desc'
-        : commentOrder === 0
-        ? 'asc'
-        : 'asc'
-    ]((comment) =>
-      commentOrder >= 0
-        ? new Date(comment.updatedAt).getTime()
-        : new Date(comment.createdAt).getTime()
+    // State to manage sorted comments
+    const [sortedComments, setSortedComments] = useState<IssueCommentType[]>(
+      [],
     );
-    
 
-    setSortedComments(sorted); // Update state with sorted comments
-  }, [commentOrder,commentsStore, commentsStore.comments.length, issue.id]);
+    const { users, isLoading } = useUsersData(true);
 
-  function getUserData(userId: string) {
-    return users.find((user: User) => user.id === userId);
-  }
+    useEffect(() => {
+      // Fetch and sort comments whenever the order or comments change
+      const comments = commentsStore.getComments(issue.id);
+      let sorted = [...comments];
+      sorted = sort(sorted)[
+        commentOrder > 0 ? 'desc' : commentOrder === 0 ? 'asc' : 'asc'
+      ]((comment) =>
+        commentOrder >= 0
+          ? new Date(comment.updatedAt).getTime()
+          : new Date(comment.createdAt).getTime(),
+      );
 
-  function getChildComments(issueCommentId: string) {
-    return sortedComments.filter(
-      (comment: IssueCommentType) => comment.parentId === issueCommentId,
+      setSortedComments(sorted); // Update state with sorted comments
+    }, [commentOrder, commentsStore, commentsStore.comments.length, issue.id]);
+
+    function getUserData(userId: string) {
+      return users.find((user: User) => user.id === userId);
+    }
+
+    function getChildComments(issueCommentId: string) {
+      return sortedComments.filter(
+        (comment: IssueCommentType) => comment.parentId === issueCommentId,
+      );
+    }
+
+    if (isLoading) {
+      return null;
+    }
+
+    return (
+      <div className="my-2 w-full flex flex-col gap-4">
+        {commentOrder == 1 && <IssueComment />}
+        <Timeline>
+          {sortedComments
+            .filter((comment: IssueCommentType) => !comment.parentId)
+            .map((comment: IssueCommentType, index: number) => (
+              <CommentActivity
+                issueId={issue.id}
+                commentId={comment.id}
+                hasMore={index > 0}
+                key={comment.id}
+                user={getUserData(comment.userId)}
+                childComments={getChildComments(comment.id)}
+                allowReply
+                getUserData={getUserData}
+              />
+            ))}
+        </Timeline>
+
+        {commentOrder < 1 && <IssueComment />}
+      </div>
     );
-  }
-
-  if (isLoading) {
-    return null;
-  }
-
-  return (
-    <div className="my-2 w-full flex flex-col gap-4">
-       {commentOrder==1 && <IssueComment />}
-       <Timeline>
-        {sortedComments
-          .filter((comment: IssueCommentType) => !comment.parentId)
-          .map((comment: IssueCommentType, index: number) => (
-            <CommentActivity
-              issueId={issue.id}
-              commentId={comment.id}
-              hasMore={index > 0}
-              key={comment.id}
-              user={getUserData(comment.userId)}
-              childComments={getChildComments(comment.id)}
-              allowReply
-              getUserData={getUserData}
-            />
-          ))}
-      </Timeline>
-
-
-      {commentOrder<1 && <IssueComment />}
-    </div>
-  );
-});
+  },
+);
