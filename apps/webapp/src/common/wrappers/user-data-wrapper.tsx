661d6385203496212f00f314e99b04536fb4da61
Harshith Mullapudi <harshithmullapudi@gmail.com>
2025-01-02 21:59:34 +0530

    Feat: suspending an account

--- apps/webapp/src/common/wrappers/user-data-wrapper.tsx
+++ apps/webapp/src/common/wrappers/user-data-wrapper.tsx
@@ -1,6 +1,12 @@
+import { Button } from '@tegonhq/ui/components/button';
+import { Logo } from '@tegonhq/ui/components/dynamic-logo';
 import { Loader } from '@tegonhq/ui/components/loader';
+import { useRouter } from 'next/router';
 import posthog from 'posthog-js';
 import * as React from 'react';
+import { signOut } from 'supertokens-auth-react/recipe/session';
+
+import { deleteCookies } from 'common/common-utils';
 
 import { useGetUserQuery } from 'services/users';
 
@@ -13,6 +19,10 @@ interface Props {
 export function UserDataWrapper(props: Props): React.ReactElement {
   const { children } = props;
   const { data, error: isError, isLoading } = useGetUserQuery();
+  const {
+    query: { workspaceSlug },
+    replace,
+  } = useRouter();
 
   React.useEffect(() => {
     if (!isLoading && !isError) {
@@ -25,6 +35,30 @@ export function UserDataWrapper(props: Props): React.ReactElement {
   }, [!isLoading, !isError]);
 
   if (!isLoading && !isError) {
+    const workspaceRes = data.workspaces.find(
+      (work) => work.slug === workspaceSlug,
+    );
+
+    if (workspaceRes.status === 'SUSPENDED') {
+      return (
+        <div className="flex flex-col h-[100vh] w-[100vw] items-center justify-center gap-2">
+          <Logo width={100} height={100} /> Your account is suspended
+          <Button
+            variant="secondary"
+            onClick={async () => {
+              posthog.reset(true);
+              deleteCookies();
+              await signOut();
+
+              replace('/auth');
+            }}
+          >
+            Logout
+          </Button>
+        </div>
+      );
+    }
+
     return <UserContext.Provider value={data}>{children}</UserContext.Provider>;
   }
 
